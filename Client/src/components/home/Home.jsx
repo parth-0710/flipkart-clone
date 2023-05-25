3ef4107fd154968d03a779d0d72445fded6da751
parth <parthtrivedi0710@gmail.com>
2023-05-25 10:49:47 +0530

    Home Page Complete

--- Client/src/components/home/Home.jsx
+++ Client/src/components/home/Home.jsx
@@ -1,6 +1,12 @@
+import {useEffect} from 'react';
+import Slide from "./Slide";
 import NavBar from "./NavBar";
 import Banner from "./Banner";
 import { Box, styled } from "@mui/material";
+import {getProducts} from "../../redux/actions/productActions";
+import { useDispatch,useSelector } from 'react-redux';
+import MidSlide from './MidSlide'
+import MidSection from './MidSection';
 
 
 
@@ -11,6 +17,15 @@ background: #F2F2F2;
 `
 
 const Home = () => {
+    const {products}=useSelector(state=>state.getProducts);
+   
+
+   
+    const dispatch=useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts());
+    },[dispatch]);
     return (
         <>
             <NavBar />
@@ -18,6 +33,15 @@ const Home = () => {
             <Component>
 
             <Banner/>
+            <MidSlide products={products} title="Deal of the Day" timer={true}/>
+            <MidSection/>
+            <Slide products={products} title="Discounts for you" timer={false}/>
+            <Slide products={products} title="Suggesting Items" timer={false}/>
+            <Slide products={products} title="Top Selection" timer={false}/>
+            <Slide products={products} title="Recommended Items" timer={false}/>
+            <Slide products={products} title="Tranding Offers" timer={false}/>
+            <Slide products={products} title="Season's top picks" timer={false}/>
+            <Slide products={products} title="Top Deals On Accessories" timer={false}/>
             </Component>
             </>
 
